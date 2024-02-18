3865936769b3d814ccad7a055ca0db6e6f3dd41a
Verónica Reina Hernández <veroreinah@gmail.com>
2024-02-18 10:28:40 +0100

    WIP. Generate sonnets from "Postales" json

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,43 @@
-const POSTALES = [
-  "https://desmusea.substack.com/p/message",
-  "https://desmusea.substack.com/p/message-1",
-  "https://desmusea.substack.com/p/message-2",
-  "https://desmusea.substack.com/p/message-3",
-  "https://desmusea.substack.com/p/message-4",
-  "https://desmusea.substack.com/p/message-5",
-  "https://desmusea.substack.com/p/message-6",
-  "https://desmusea.substack.com/p/message-7",
-  "https://desmusea.substack.com/p/message-8",
-  "https://desmusea.substack.com/p/message-9",
-];
+const shuffleArray = (arr) =>
+  arr
+    .map((a) => [Math.random(), a])
+    .sort((a, b) => a[0] - b[0])
+    .map((a) => a[1]);
 
 (async function getData() {
-  const myHeaders = new Headers();
-  const requestOptions = {
-    method: "GET",
-    headers: myHeaders,
-    redirect: "follow",
-    mode: "cors",
-  };
+  fetch("/data/postales.json")
+    .then((response) => response.json())
+    .then((result) => {
+      if (Array.isArray(result)) {
+        const postales = shuffleArray(result).slice(0, 140);
+        const main = document.getElementsByTagName("main").item(0);
 
-  fetch(POSTALES[0], requestOptions)
-    .then((response) => response.text())
-    .then((result) => console.log(result))
+        let count = 0;
+
+        const addNode = (article, data) => {
+          const paragraph = document.createElement("p");
+          const node = document.createTextNode(data);
+          paragraph.appendChild(node);
+          article.appendChild(paragraph);
+        };
+
+        for (let index = 0; index < 10; index++) {
+          const article = document.createElement("article");
+
+          for (let index = 0; index < 14; index++) {
+            const addEmptyLine = index === 4 || index === 8 || index === 11;
+
+            if (addEmptyLine) {
+              addNode(article, "\u00A0");
+            }
+
+            addNode(article, postales[count]);
+            count++;
+          }
+
+          main.appendChild(article);
+        }
+      }
+    })
     .catch((error) => console.log("error", error));
 })();
